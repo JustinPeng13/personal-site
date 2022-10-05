32ff8f9a241e2ba65ab57f8a7b59ac181790f8e0
Justin Peng <justinpeng13@gmail.com>
2022-10-05 23:07:17 +0800

    Add pulse animation on hover

--- components/Conic.tsx
+++ components/Conic.tsx
@@ -11,6 +11,20 @@ const ConicDiv = styled.div`
     }
   }
 
+  @keyframes pulse { 
+    0% {
+      opacity: 0.1;
+    }
+  
+    50% {
+      opacity: 1;
+    }
+
+    100% {
+      opacity: 0.1;
+    }
+  }
+
   position: relative;
   z-index: 0;
   border-radius: 10px;
@@ -26,11 +40,17 @@ const ConicDiv = styled.div`
     width: 250%;
     height: 250%;
     background-color: ${props => props.dark ? 'rgb(17, 24, 39);' : 'rgb(250, 250, 250);'}
-    // background-color: rgb(250, 250, 250);
     background-repeat: no-repeat;
     background-position: 0 0;
     background-image: conic-gradient(transparent, rgba(99, 102, 241, 1), transparent 75%);
     animation: rotate 5s linear infinite;
+    transition: 0.5s;
+  }
+
+  :hover::before {
+    background-image: none;
+    background-color: rgba(99, 102, 241, 1);
+    animation: pulse 2s cubic-bezier(.5,.15,.85,.5) infinite;
   }
     
   &::after {
@@ -41,7 +61,7 @@ const ConicDiv = styled.div`
     top: 6px;
     width: calc(100% - 12px);
     height: calc(100% - 12px);
-    // background: #000;
+    background: #000;
     border-radius: 5px;
   }
 `
